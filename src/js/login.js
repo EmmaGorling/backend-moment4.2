9daf555768e39450865defb3eda77f6ca3bf4365
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-05-05 10:03:08 +0200

    Har skapat de sidor jag tror behövs Har på börjat login funktioner, men löser det inte riktigt... Får klura lite

---
+++ src/js/login.js
@@ -0,0 +1,42 @@
+"use strict"
+
+const errorDiv = document.getElementById('error');
+const url = 'https://emad2301-backend-moment4.onrender.com'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('loginForm');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Hindrar att formuläret skickas traditionellt
+        loginUser();
+    })
+});
+
+// Function for logging in with fetch-POST
+async function loginUser() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const user = {
+        email: email,
+        password: password
+    }
+
+    try {
+        const response = await fetch(url + '/api/login', {
+            method: 'POST',
+            headers: {
+                "content-type": "Application/json"
+            },
+            body: JSON.stringify(user)
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+}
