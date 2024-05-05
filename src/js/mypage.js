e9e15febddc6526df6fe4e2ad8c957ce87a0c443
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-05-05 17:08:05 +0200

    Har fått till funktionaliteten, styling kvar

--- src/js/mypage.js
+++ src/js/mypage.js
@@ -1,3 +1,32 @@
 "use strict"
 
-console.log('Hej');
+const url = 'https://emad2301-backend-moment4.onrender.com';
+
+window.onload = init;
+
+function init() {
+    if(!localStorage.getItem('token')) {
+        window.location.href = 'login.html';
+    }
+
+    contactApi();
+}
+
+async function contactApi() {
+    // Get token from localStorage
+    const token = localStorage.getItem('token');
+
+    // Fetch to protected route
+    try {
+        const response = await fetch(url + '/api/protected', {
+            headers: {
+                "Authorization": 'Bearer ' + token
+            }
+        });
+        const data = await response.json();
+
+        document.getElementById('message').innerHTML = data.message;
+    } catch (error) {
+        console.log(error);
+    }
+}
